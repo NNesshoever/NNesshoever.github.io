2f8d910e8c6277b1395c20e6f87a015c0cdf863c
Niclas Neßhöver <niclasnesshoever@gmail.com>
2023-10-27 16:07:58 +0200

    TestFront-Controller

--- frontcontroller/frontcontroller.js
+++ frontcontroller/frontcontroller.js
@@ -1,9 +1,24 @@
 let relativePath = window.location.pathname.split("/").pop()
-switch(relativePath){
-    case 'test':
-        console.log("Test");
-        break;
+let anchor = document.getElementById("html")
+let test = true;
+if(test){
+
+}else{
+    switch(relativePath){
+        case 'test':
+            console.log("Test");
+            break;
+        case 'businessCard':
+            filePath = "../businessCard.html"
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", filePath, false)
+            xmlhttp.send()
+            let html = xmlhttp.responseText
+            anchor.innerHTML = html
+            break
     
-    default:
-        window.localStorage.href = "nnesshoever.github.io"
+        default:
+            window.localStorage.href = "nnesshoever.github.io"
+    }
 }
+
